17c6ca445d0bcfc60983079307b24513167366b8
devwesolowski <dev.wesolowski@gmail.com>
2023-03-22 14:00:36 -0500

    created player and choice variables

---
+++ rps.js
@@ -0,0 +1,4 @@
+var player1Name = "Jake";
+var player2Name = "Dane";
+var player1Choice;
+var player2Choice;
---
+++ rps.ts
@@ -0,0 +1,4 @@
+const player1Name: string = "Jake";
+const player2Name: string = "Dane";
+let player1Choice: string;
+let player2Choice: string;
