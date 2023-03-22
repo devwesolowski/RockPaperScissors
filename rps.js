f43620054d4e50010a9266e56cd19bfca54b3a29
devwesolowski <dev.wesolowski@gmail.com>
2023-03-22 14:31:28 -0500

    players set to their choices and decision logged

--- rps.js
+++ rps.js
@@ -6,3 +6,7 @@ var tools = ["rock", "paper", "scissors"];
 function setPlayerChoice(tools) {
     return tools[Math.floor(Math.random() * tools.length)];
 }
+player1Choice = setPlayerChoice(tools);
+player2Choice = setPlayerChoice(tools);
+console.log("".concat(player1Name, " played ").concat(player1Choice));
+console.log("".concat(player2Name, " played ").concat(player2Choice));
--- rps.ts
+++ rps.ts
@@ -8,3 +8,8 @@ const tools: Array<string> = ["rock", "paper", "scissors"];
 function setPlayerChoice(tools: Array<string>): string {
   return tools[Math.floor(Math.random() * tools.length)];
 }
+
+player1Choice = setPlayerChoice(tools);
+player2Choice = setPlayerChoice(tools);
+console.log(`${player1Name} played ${player1Choice}`);
+console.log(`${player2Name} played ${player2Choice}`);
