e872fbe6e4f802d8e2f5cdf47e987b8a8b5bdf72
devwesolowski <dev.wesolowski@gmail.com>
2023-03-22 14:28:27 -0500

    implemented tool array and function to set player choice

--- rps.js
+++ rps.js
@@ -2,3 +2,7 @@ var player1Name = "Jake";
 var player2Name = "Dane";
 var player1Choice;
 var player2Choice;
+var tools = ["rock", "paper", "scissors"];
+function setPlayerChoice(tools) {
+    return tools[Math.floor(Math.random() * tools.length)];
+}
--- rps.ts
+++ rps.ts
@@ -2,3 +2,9 @@ const player1Name: string = "Jake";
 const player2Name: string = "Dane";
 let player1Choice: string;
 let player2Choice: string;
+
+const tools: Array<string> = ["rock", "paper", "scissors"];
+
+function setPlayerChoice(tools: Array<string>): string {
+  return tools[Math.floor(Math.random() * tools.length)];
+}
